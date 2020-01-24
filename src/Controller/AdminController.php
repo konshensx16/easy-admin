52ad3df83461452ff9e83536fc257615e53ce70d
Overseas Media <konshensx16@gmail.com>
2020-01-24 22:39:22 +0100

    [Listing] Create the custom controller to get records.

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\Common\Collections\Criteria;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+
+class AdminController extends EasyAdminController
+{
+    public function recordsAction()
+    {
+        // get all the criteria for the search
+        $search = $this->request->get('search', null);
+        $order = $this->request->get('order', null);
+        $offset = $this->request->get('offset', null);
+        $limit = $this->request->get('limit', null);
+        // get the searchable fields
+        $class = $this->entity['class'];
+        $em = $this->getDoctrine()->getManagerForClass($class);
+        $searchableFields = array_keys($this->entity['search']['fields']);
+        if ($search != "") {
+            // FIXME:
+            $criteria = new Criteria();
+            foreach ($searchableFields as $field) {
+                $criteria->orWhere($criteria->expr()->contains($field, $search));
+            }
+            if ($order) {
+                $criteria->orderBy(['id' => $order]);
+            }
+            if (!is_null($offset)) {
+                $criteria->setFirstResult($offset);
+            }
+            if ($limit) {
+                $criteria->setMaxResults($limit);
+            }
+            $records = $em->getRepository($class)->matching($criteria);
+        } else {
+            $records = $em->getRepository($class)->findAll();
+        }
+
+        // fields to serialize
+        $fields = $this->entity['list']['fields'];
+        $fields['id'] = null;
+
+        $fields = array_map(function ($field) {
+            if (strpos($field, '.') !== false) {
+                return explode('.', $field)[0];
+            }
+            return $field;
+        }, array_keys($fields));
+
+
+        // Serialize the data
+        return $this->json([
+            'row' => $records,
+            'total' => count($records),
+            'totalNotFiltered' => count($records) // FIXME: get the proper total
+        ], 200, [], [
+            'attributes' => $fields,
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]);
+    }
+}
